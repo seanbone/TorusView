8696441dddfe4bf7d83839ec615ec11ebc90d274
Sean Bone <seanbone@zumguy.com>
2021-02-11 12:51:21 +0100

    Removed build directory

--- build/external/glm/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sean/ETH/fs21/OpenGL/TorusView/external/glm
-# Build directory: /home/sean/ETH/fs21/OpenGL/TorusView/build/external/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("glm")
